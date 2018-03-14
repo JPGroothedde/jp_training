1f181d4c73130076963a39969af312ce4cf8c3da
JPGroothedde <37298441+JPGroothedde@users.noreply.github.com>
2018-03-14 12:36:03 +0200

    Update task_3.php

--- task_3.php
+++ task_3.php
@@ -5,12 +5,6 @@
  * Date: 2018/03/13
  * Time: 8:20 AM
  */
-//Task:
-//A palindrome is a word that reads the same backward or forward.
-//Write a function that checks if a given word is a palindrome. Character case should be ignored.
-//For example, isPalindrome("Never Odd Or Even") should return true as character case should be ignored, resulting in "Never Odd Or Even",
-// which is a palindrome since it reads the same backward and forward.
-
 class Palindrome {
     public static function isPalindrome($word) {
 //TODO: Implement this
